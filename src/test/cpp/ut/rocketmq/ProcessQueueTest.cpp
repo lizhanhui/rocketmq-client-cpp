f02e92914168fa74791479bc06644a4fb14fd8cb
Li Zhanhui <lizhanhui@gmail.com>
2021-07-31 22:57:17 +0800

    Add unit tests for ProcessQueue

--- src/test/cpp/ut/rocketmq/ProcessQueueTest.cpp
+++ src/test/cpp/ut/rocketmq/ProcessQueueTest.cpp
@@ -1,19 +1,22 @@
 #include "Assignment.h"
 #include "ClientInstance.h"
 #include "ClientManager.h"
-#include "DefaultMQPushConsumerImpl.h"
 #include "InvocationContext.h"
+#include "MessageAccessor.h"
 #include "ProcessQueueImpl.h"
-#include "ProcessQueueMock.h"
+#include "PushConsumerMock.h"
 #include "ReceiveMessageCallbackMock.h"
 #include "RpcClientMock.h"
 #include "absl/memory/memory.h"
 #include "absl/synchronization/mutex.h"
-#include "gtest/gtest.h"
 #include "apache/rocketmq/v1/definition.pb.h"
+#include "rocketmq/MQMessageExt.h"
+#include "gtest/gtest.h"
 #include <chrono>
 #include <iostream>
 #include <memory>
+#include <string>
+#include <vector>
 
 ROCKETMQ_NAMESPACE_BEGIN
 
@@ -29,12 +32,10 @@ public:
     ClientManager::getInstance().addClientInstance(arn_, client_instance_);
     client_instance_->addRpcClient(service_address_, rpc_client_);
     auto credentials_provider = std::make_shared<StaticCredentialsProvider>(access_key_, access_secret_);
-    consumer_ = std::make_shared<DefaultMQPushConsumerImpl>(group_name_);
-    consumer_->setCredentialsProvider(credentials_provider);
-    consumer_->arn(arn_);
-    consumer_->region(region_);
+    consumer_ = std::make_shared<testing::NiceMock<PushConsumerMock>>();
+    auto consumer = std::dynamic_pointer_cast<PushConsumer>(consumer_);
     process_queue_ = std::make_shared<ProcessQueueImpl>(message_queue_, filter_expression_, ConsumeMessageType::POP,
-                                                    consumer_, client_instance_);
+                                                        consumer, client_instance_);
     receive_message_callback_ = std::make_shared<testing::NiceMock<ReceiveMessageCallbackMock>>();
     process_queue_->callback(receive_message_callback_);
   }
@@ -50,15 +51,20 @@ protected:
   int queue_id_{0};
   std::string topic_{"TestTopic"};
   std::string service_address_{"ipv4:10.0.0.1:10911"};
-  FilterExpression filter_expression_{"TagA"};
+  std::string tag_{"TagA"};
+  FilterExpression filter_expression_{tag_};
   MQMessageQueue message_queue_;
   std::shared_ptr<testing::NiceMock<RpcClientMock>> rpc_client_;
   std::shared_ptr<ClientInstance> client_instance_;
-  std::shared_ptr<DefaultMQPushConsumerImpl> consumer_;
+  std::shared_ptr<testing::NiceMock<PushConsumerMock>> consumer_;
   std::shared_ptr<ProcessQueueImpl> process_queue_;
   std::shared_ptr<testing::NiceMock<ReceiveMessageCallbackMock>> receive_message_callback_;
   std::string arn_{"arn:test"};
   std::string message_body_{"Sample body"};
+
+  uint32_t threshold_quantity_{32};
+  uint64_t threshold_memory_{4096};
+  uint32_t consume_batch_size_{8};
 };
 
 TEST_F(ProcessQueueTest, testBind) {
@@ -75,67 +81,108 @@ TEST_F(ProcessQueueTest, testExpired) {
   EXPECT_TRUE(process_queue_->expired());
 }
 
-TEST_F(ProcessQueueTest, testReceiveMessageByPop) {
-  absl::Mutex mtx;
-  absl::CondVar cv;
-  bool completed = false;
-  auto callback = [&](const ReceiveMessageRequest& request,
-                      InvocationContext<ReceiveMessageResponse>* invocation_context) {
-    invocation_context->onCompletion(true);
-    absl::MutexLock lk(&mtx);
-    completed = true;
-    cv.SignalAll();
-  };
-  EXPECT_CALL(*rpc_client_, asyncReceive).Times(testing::AtLeast(1)).WillRepeatedly(testing::Invoke(callback));
-  EXPECT_CALL(*rpc_client_, ok).Times(testing::AtLeast(1)).WillRepeatedly(testing::Return(true));
-  EXPECT_CALL(*receive_message_callback_, onSuccess).Times(testing::AtLeast(1));
-  process_queue_->receiveMessage();
-
-  while (!completed) {
-    absl::MutexLock lk(&mtx);
-    cv.Wait(&mtx);
-  }
+TEST_F(ProcessQueueTest, testShouldThrottle) {
+  EXPECT_CALL(*consumer_, maxCachedMessageQuantity)
+      .Times(testing::AtLeast(1))
+      .WillRepeatedly(testing::Return(threshold_quantity_));
+  EXPECT_CALL(*consumer_, maxCachedMessageMemory)
+      .Times(testing::AtLeast(1))
+      .WillRepeatedly(testing::Return(threshold_memory_));
+  EXPECT_FALSE(process_queue_->shouldThrottle());
 }
 
-TEST_F(ProcessQueueTest, testReceiveMessageByPull) {
-  process_queue_->consumeType(ConsumeMessageType::PULL);
-  absl::Mutex mtx;
-  absl::CondVar cv;
-  bool completed = false;
-  auto callback = [&](const PullMessageRequest& request, InvocationContext<PullMessageResponse>* invocation_context) {
-    invocation_context->response.set_min_offset(0);
-    invocation_context->response.set_next_offset(33);
-    invocation_context->response.set_max_offset(64);
+TEST_F(ProcessQueueTest, testShouldThrottle_ByQuantity) {
+  std::vector<MQMessageExt> messages;
+  for (uint32_t i = 0; i < threshold_quantity_; i++) {
+    MQMessageExt message;
+    message.setTopic(topic_);
+    message.setTags(tag_);
+    MessageAccessor::setQueueId(message, 0);
+    MessageAccessor::setQueueOffset(message, i);
+    message.setBody(std::to_string(i));
+    messages.emplace_back(message);
+  }
 
-    for (int32_t i = 0; i < request.batch_size(); i++) {
-      auto message = new rmq::Message;
-      message->set_body(message_body_);
-      message->mutable_topic()->set_arn(request.partition().topic().arn());
-      message->mutable_topic()->set_name(request.partition().topic().name());
-      message->mutable_system_attribute()->mutable_body_digest()->set_type(rmq::DigestType::SHA1);
-      std::string digest;
-      MixAll::sha1(message_body_, digest);
-      message->mutable_system_attribute()->mutable_body_digest()->set_checksum(digest);
-      invocation_context->response.mutable_messages()->AddAllocated(message);
+  process_queue_->cacheMessages(messages);
+
+  EXPECT_CALL(*consumer_, maxCachedMessageQuantity)
+      .Times(testing::AtLeast(1))
+      .WillRepeatedly(testing::Return(threshold_quantity_));
+  EXPECT_CALL(*consumer_, maxCachedMessageMemory)
+      .Times(testing::AtLeast(1))
+      .WillRepeatedly(testing::Return(threshold_memory_));
+  EXPECT_TRUE(process_queue_->shouldThrottle());
+}
+
+TEST_F(ProcessQueueTest, testShouldThrottle_ByMemory) {
+  std::vector<MQMessageExt> messages;
+  size_t body_length = 1024 * 4;
+  for (uint32_t i = 0; i < threshold_quantity_ / 2; i++) {
+    MQMessageExt message;
+    message.setTopic(topic_);
+    message.setTags(tag_);
+    MessageAccessor::setQueueId(message, 0);
+    MessageAccessor::setQueueOffset(message, i);
+    message.setBody(std::string(body_length, 'c'));
+    messages.emplace_back(message);
+  }
+
+  process_queue_->cacheMessages(messages);
+
+  EXPECT_CALL(*consumer_, maxCachedMessageQuantity)
+      .Times(testing::AtLeast(1))
+      .WillRepeatedly(testing::Return(threshold_quantity_));
+  EXPECT_CALL(*consumer_, maxCachedMessageMemory)
+      .Times(testing::AtLeast(1))
+      .WillRepeatedly(testing::Return(threshold_memory_));
+  EXPECT_TRUE(process_queue_->shouldThrottle());
+}
+
+TEST_F(ProcessQueueTest, testHasPendingMessages) { EXPECT_FALSE(process_queue_->hasPendingMessages()); }
+
+TEST_F(ProcessQueueTest, testHasPendingMessages2) {
+  std::vector<MQMessageExt> messages;
+  size_t body_length = 1024;
+  for (size_t i = 0; i < threshold_quantity_; i++) {
+    MQMessageExt message;
+    message.setTopic(topic_);
+    message.setTags(tag_);
+    MessageAccessor::setQueueId(message, 0);
+    MessageAccessor::setQueueOffset(message, i);
+    message.setBody(std::string(body_length, 'c'));
+    messages.emplace_back(message);
+  }
+  process_queue_->cacheMessages(messages);
+  EXPECT_TRUE(process_queue_->hasPendingMessages());
+}
+
+TEST_F(ProcessQueueTest, testTake) {
+  std::vector<MQMessageExt> messages;
+  EXPECT_FALSE(process_queue_->take(consume_batch_size_, messages));
+  EXPECT_TRUE(messages.empty());
+}
+
+TEST_F(ProcessQueueTest, testTake2) {
+
+  {
+    std::vector<MQMessageExt> messages;
+    size_t body_length = 1024;
+    for (size_t i = 0; i < threshold_quantity_; i++) {
+      MQMessageExt message;
+      message.setTopic(topic_);
+      message.setTags(tag_);
+      MessageAccessor::setQueueId(message, 0);
+      MessageAccessor::setQueueOffset(message, i);
+      message.setBody(std::string(body_length, 'c'));
+      messages.emplace_back(message);
     }
-    invocation_context->onCompletion(true);
-    absl::MutexLock lk(&mtx);
-    completed = true;
-    cv.SignalAll();
-  };
-  EXPECT_CALL(*rpc_client_, asyncPull).Times(testing::AtLeast(1)).WillRepeatedly(testing::Invoke(callback));
-  EXPECT_CALL(*rpc_client_, ok).Times(testing::AtLeast(1)).WillRepeatedly(testing::Return(true));
-  EXPECT_CALL(*receive_message_callback_, onSuccess).Times(testing::AtLeast(1));
-  process_queue_->receiveMessage();
-
-  while (!completed) {
-    absl::MutexLock lk(&mtx);
-    cv.Wait(&mtx);
+    process_queue_->cacheMessages(messages);
   }
-}
 
-TEST(ProcessQueueMockTest, testMock) {
-  testing::NiceMock<ProcessQueueMock> mock;
+  std::vector<MQMessageExt> msgs;
+  EXPECT_TRUE(process_queue_->take(consume_batch_size_, msgs));
+  EXPECT_FALSE(msgs.empty());
+  // EXPECT_EQ(tag_, msgs.begin()->getTags());
 }
 
 ROCKETMQ_NAMESPACE_END
