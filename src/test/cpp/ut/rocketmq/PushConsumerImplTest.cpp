f7128448f56520fb665c9a777892d2a02471f14a
Li Zhanhui <lizhanhui@gmail.com>
2021-08-03 15:43:34 +0800

    Add unit test for PushConsumerImpl

--- src/test/cpp/ut/rocketmq/PushConsumerImplTest.cpp
+++ src/test/cpp/ut/rocketmq/PushConsumerImplTest.cpp
@@ -1,6 +1,7 @@
 #include "PushConsumerImpl.h"
 #include "ClientManagerFactory.h"
 #include "ClientManagerMock.h"
+#include "InvocationContext.h"
 #include "grpc/grpc.h"
 #include "rocketmq/MQMessageExt.h"
 #include "rocketmq/RocketMQ.h"
@@ -17,13 +18,9 @@ class PushConsumerImplTest : public testing::Test {
     push_consumer_ = std::make_shared<PushConsumerImpl>(group_);
     push_consumer_->arn(arn_);
     push_consumer_->setNameServerList(name_server_list_);
-    push_consumer_->start();
   }
 
-  void TearDown() override {
-    push_consumer_->shutdown();
-    grpc_shutdown();
-  }
+  void TearDown() override { grpc_shutdown(); }
 
 protected:
   std::vector<std::string> name_server_list_{"10.0.0.1:9876"};
@@ -39,10 +36,15 @@ protected:
 };
 
 TEST_F(PushConsumerImplTest, testAck) {
+  Scheduler scheduler;
+  ON_CALL(*client_manager_, getScheduler).WillByDefault(testing::ReturnRef(scheduler));
+
   auto ack_cb = [](const std::string& target_host, const Metadata& metadata, const AckMessageRequest& request,
                    std::chrono::milliseconds timeout, const std::function<void(bool)>& cb) { cb(true); };
 
   EXPECT_CALL(*client_manager_, ack).Times(testing::AtLeast(1)).WillRepeatedly(testing::Invoke(ack_cb));
+
+  push_consumer_->start();
 
   bool completed = false;
   absl::Mutex mtx;
@@ -69,6 +71,93 @@ TEST_F(PushConsumerImplTest, testAck) {
     }
   }
   EXPECT_TRUE(completed);
+  push_consumer_->shutdown();
+}
+
+TEST_F(PushConsumerImplTest, testNack) {
+  Scheduler scheduler;
+  ON_CALL(*client_manager_, getScheduler).WillByDefault(testing::ReturnRef(scheduler));
+
+  auto nack_cb = [](const std::string& target_host, const Metadata& metadata, const NackMessageRequest& request,
+                    std::chrono::milliseconds timeout, const std::function<void(bool)>& cb) { cb(true); };
+
+  EXPECT_CALL(*client_manager_, nack).Times(testing::AtLeast(1)).WillRepeatedly(testing::Invoke(nack_cb));
+
+  push_consumer_->start();
+
+  bool completed = false;
+  absl::Mutex mtx;
+  absl::CondVar cv;
+  auto callback = [&](bool ok) {
+    absl::MutexLock lk(&mtx);
+    completed = true;
+    cv.SignalAll();
+  };
+
+  MQMessageExt message;
+  message.setTopic(topic_);
+  message.setBody(message_body_);
+  message.setTags(tag_);
+  message.setKey(key_);
+  message.setDelayTimeLevel(delay_level_);
+
+  push_consumer_->nack(message, callback);
+
+  {
+    absl::MutexLock lk(&mtx);
+    if (!completed) {
+      cv.WaitWithDeadline(&mtx, absl::Now() + absl::Seconds(3));
+    }
+  }
+  EXPECT_TRUE(completed);
+  push_consumer_->shutdown();
+}
+
+TEST_F(PushConsumerImplTest, testForward) {
+  Scheduler scheduler;
+  ON_CALL(*client_manager_, getScheduler).WillByDefault(testing::ReturnRef(scheduler));
+
+  InvocationContext<ForwardMessageToDeadLetterQueueResponse> invocation_context;
+
+  auto forward_cb =
+      [&](const std::string& target_host, const Metadata& metadata,
+          const ForwardMessageToDeadLetterQueueRequest& request, std::chrono::milliseconds timeout,
+          const std::function<void(const InvocationContext<ForwardMessageToDeadLetterQueueResponse>*)>& cb) {
+        cb(&invocation_context);
+      };
+
+  EXPECT_CALL(*client_manager_, forwardMessageToDeadLetterQueue)
+      .Times(testing::AtLeast(1))
+      .WillRepeatedly(testing::Invoke(forward_cb));
+
+  push_consumer_->start();
+
+  bool completed = false;
+  absl::Mutex mtx;
+  absl::CondVar cv;
+  auto callback = [&](bool ok) {
+    absl::MutexLock lk(&mtx);
+    completed = true;
+    cv.SignalAll();
+  };
+
+  MQMessageExt message;
+  message.setTopic(topic_);
+  message.setBody(message_body_);
+  message.setTags(tag_);
+  message.setKey(key_);
+  message.setDelayTimeLevel(delay_level_);
+
+  push_consumer_->forwardToDeadLetterQueue(message, callback);
+
+  {
+    absl::MutexLock lk(&mtx);
+    if (!completed) {
+      cv.WaitWithDeadline(&mtx, absl::Now() + absl::Seconds(3));
+    }
+  }
+  EXPECT_TRUE(completed);
+  push_consumer_->shutdown();
 }
 
 ROCKETMQ_NAMESPACE_END
