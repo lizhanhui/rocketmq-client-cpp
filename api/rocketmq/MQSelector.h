129f060bc77663c4621ccbcead2282e3ad9afd8d
Li Zhanhui <lizhanhui@gmail.com>
2021-09-23 11:33:30 +0800

    Fix include order

--- api/rocketmq/MQSelector.h
+++ api/rocketmq/MQSelector.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <vector>
-#include "MQMessageQueue.h"
+
 #include "MQMessage.h"
+#include "MQMessageQueue.h"
 
 ROCKETMQ_NAMESPACE_BEGIN
 
 class MessageQueueSelector {
 public:
-    virtual ~MessageQueueSelector() {}
-    virtual MQMessageQueue select(const std::vector<MQMessageQueue>& mqs, const MQMessage& msg, void* arg) = 0;
+  virtual ~MessageQueueSelector() {}
+  virtual MQMessageQueue select(const std::vector<MQMessageQueue>& mqs, const MQMessage& msg, void* arg) = 0;
 };
 
 ROCKETMQ_NAMESPACE_END
