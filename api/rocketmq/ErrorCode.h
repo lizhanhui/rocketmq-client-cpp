d3316ff19e50e7584a4d15e17b09bbc43512b4e4
Li Zhanhui <lizhanhui@gmail.com>
2021-09-25 07:26:59 +0800

    Polish code

--- api/rocketmq/ErrorCode.h
+++ api/rocketmq/ErrorCode.h
@@ -143,99 +143,7 @@ enum class ErrorCode : int {
   InsufficientStorage = 507,
 };
 
-class ErrorCategory : public std::error_category {
-public:
-  static const ErrorCategory &instance() {
-    static ErrorCategory instance;
-    return instance;
-  }
-
-  const char *name() const noexcept override { return "RocketMQ"; }
-
-  std::string message(int code) const override {
-    ErrorCode ec = static_cast<ErrorCode>(code);
-    switch (ec) {
-    case ErrorCode::Success:
-      return "Success";
-
-    case ErrorCode::IllegalState:
-      return "Client state illegal. Forgot to call start()?";
-
-    case ErrorCode::BadConfiguration:
-      return "Bad configuration.";
-
-    case ErrorCode::BadRequest:
-      return "Message is ill-formed. Check validity of your topic, tag, "
-             "etc";
-
-    case ErrorCode::Unauthorized:
-      return "Authentication failed. Possibly caused by invalid credentials.";
-
-    case ErrorCode::Forbidden:
-      return "Authenticated user does not have privilege to perform the "
-             "requested action";
-
-    case ErrorCode::NotFound:
-      return "Topic not found, which should be created through console or "
-             "administration API before hand.";
-
-    case ErrorCode::RequestTimeout:
-      return "Timeout when connecting, reading from or writing to brokers.";
-
-    case ErrorCode::PayloadTooLarge:
-      return "Message body is too large.";
-
-    case ErrorCode::PreconditionRequired:
-      return "State of dependent procedure is not right";
-
-    case ErrorCode::TooManyRequest:
-      return "Quota exchausted. The user has sent too many requests in a given "
-             "amount of time.";
-
-    case ErrorCode::UnavailableForLegalReasons:
-      return "A server operator has received a legal demand to deny access to "
-             "a resource or to a set of resources that "
-             "includes the requested resource.";
-
-    case ErrorCode::HeaderFieldsTooLarge:
-      return "The server is unwilling to process the request because either an "
-             "individual header field, or all the header fields collectively, "
-             "are too large";
-
-    case ErrorCode::InternalServerError:
-      return "Server side interval error";
-
-    case ErrorCode::NotImplemented:
-      return "The server either does not recognize the request method, or it "
-             "lacks the ability to fulfil the request.";
-
-    case ErrorCode::BadGateway:
-      return "The server was acting as a gateway or proxy and received an "
-             "invalid response from the upstream server.";
-
-    case ErrorCode::ServiceUnavailable:
-      return "The server cannot handle the request (because it is overloaded "
-             "or down for maintenance). Generally, this "
-             "is a temporary state.";
-
-    case ErrorCode::GatewayTimeout:
-      return "The server was acting as a gateway or proxy and did not receive "
-             "a timely response from the upstream "
-             "server.";
-
-    case ErrorCode::ProtocolVersionNotSupported:
-      return "The server does not support the protocol version used in the "
-             "request.";
-
-    case ErrorCode::InsufficientStorage:
-      return "The server is unable to store the representation needed to "
-             "complete the request.";
-
-    default:
-      return "Not-Implemented";
-    }
-  }
-};
+std::error_code make_error_code(ErrorCode code);
 
 constexpr int NO_TOPIC_ROUTE_INFO = -1;
 
@@ -260,14 +168,6 @@ constexpr int ERR_INVALID_MAX_ATTEMPT_TIME = -10;
 ROCKETMQ_NAMESPACE_END
 
 namespace std {
-
 template <>
 struct is_error_code_enum<ROCKETMQ_NAMESPACE::ErrorCode> : true_type {};
-
 } // namespace std
-
-ROCKETMQ_NAMESPACE_BEGIN
-
-std::error_code make_error_code(ROCKETMQ_NAMESPACE::ErrorCode code);
-
-ROCKETMQ_NAMESPACE_END
--- src/main/cpp/base/ErrorCode.cpp
+++ src/main/cpp/base/ErrorCode.cpp
@@ -1,8 +1,8 @@
-#include "rocketmq/ErrorCode.h"
+#include "rocketmq/ErrorCategory.h"
 
 ROCKETMQ_NAMESPACE_BEGIN
 
-std::error_code make_error_code(ROCKETMQ_NAMESPACE::ErrorCode code) {
+std::error_code make_error_code(ErrorCode code) {
   const ErrorCategory& instance = ErrorCategory::instance();
   return {static_cast<int>(code), instance};
 }
